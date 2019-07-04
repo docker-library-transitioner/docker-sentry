f5e429deff8f8ca59ec7e8cbd4d986c181a5793a
Burak Yigit Kaya <ben@byk.im>
2019-07-04 13:05:59 +0300

    fix test runner, lol docs

--- git/run_tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-docker run --rm \
-    -e SENTRY_SKIP_BACKEND_VALIDATION=1 \
-    -e SENTRY_SECRET_KEY=abc \
-    -e SENTRY_REDIS_HOST=localhost \
-    "$1" config get system.secret-key
